ca0b03aa8ec04ca44f380b08e1495f60a159bec6
Stephen Zweibel <oneperstephen@gmail.com>
2021-12-21 20:13:05 -0500

    again

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const withYAML = require('next-yaml')
 const withPlugins = require('next-compose-plugins')
 const withMDX = require('@next/mdx')({
     extension: /\.(md|mdx)$/,
-  })
-
+})
+const repoName = 'DHRI-static'
 const debug = process.env.NODE_ENV !== "production";
 console.log(`Running in ${process.env.NODE_ENV} mode`);
 const isGitHub = process.env.GITHUB_ACTIONS === "true";
@@ -11,14 +11,15 @@ console.log(`Running in ${isGitHub ? "GitHub Actions" : "local"} mode`);
 const nextConfig = {
     // reactStrictMode: true,
     trailingSlash: true,
-    basePath: isGitHub ? '/DHRI-static' : '',
-    assetPrefix: isGitHub ? '/DHRI-static/' : '',
+    basePath: isGitHub ? '/' + repoName : '',
+    assetPrefix: isGitHub ? '/' + repoName + '/' : '',
     images: isGitHub ? {
-        path: `${basePath}/_next/image`} : null,
+        path: '/' + repoName + '/_next/image',
+    } : null,
 }
 console.log(`nextConfig: ${JSON.stringify(nextConfig)}`);
 module.exports = withPlugins([
     [withMDX],
     [withYAML],
 ],
-nextConfig)
+    nextConfig)
