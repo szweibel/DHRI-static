5a8a9a28f0bc5299d2d239f53de15b47bc49f8cd
Stephen Zweibel <oneperstephen@gmail.com>
2022-01-11 12:30:35 -0500

    test

--- pages/_app.js
+++ pages/_app.js
@@ -13,15 +13,16 @@ const Footer = dynamic(() => import('../components/Footer'))
 
 function MyApp({ Component, pageProps }) {
   const base = '/' + process.env.NEXT_PUBLIC_REPO_NAME
+  console.log(process.env.NEXT_PUBLIC_GITHUB_ACTIONS)
   return (
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link href="//cdn.muicss.com/mui-0.10.3/css/mui.min.css" rel="stylesheet" type="text/css" media="screen" />
         <title>DHRI Curriculum</title>
-        {process.env.NEXT_PUBLIC_GITHUB_ACTIONS && <><link rel="icon" href={base + "/images/favicon.ico"} sizes="32x32"></link>
+        {process.env.NEXT_PUBLIC_GITHUB_ACTIONS == false && <><link rel="icon" href={base + "/images/favicon.ico"} sizes="32x32"></link>
         <link rel="icon" href={base + "/images/favicon.ico"} sizes="192x192"></link>
-        <link rel="apple-touch-icon" href={base + "/images/favicon.ico"}></link></>}
+        <link rel="apple-touch-icon" href={base + "/images/favicon.ico" }></link></>}
       </Head>
       <CssBaseline />
       <ThemeProvider>
