a65c12e99ccdd7db7688cfc4ae8054cd35e82833
Stephen Zweibel <oneperstephen@gmail.com>
2022-03-24 09:53:27 -0400

    more darkmode

--- components/Header.js
+++ components/Header.js
@@ -15,57 +15,8 @@ import ButtonAppBarCollapse from "./ButtonAppBarCollapse";
 import { useTheme } from 'next-themes'
 import Switch from '@mui/material/Switch';
 import IconButton from '@mui/material/IconButton';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
-
-
-const MaterialUISwitch = styled(Switch)(({ theme }) => (
-    {
-        width: 62,
-        height: 34,
-        padding: 7,
-        '& .MuiSwitch-switchBase': {
-            margin: 1,
-            padding: 0,
-            transform: 'translateX(6px)',
-            '&.Mui-checked': {
-                color: '#fff',
-                transform: 'translateX(22px)',
-                '& .MuiSwitch-thumb:before': {
-                    backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-                        '#fff',
-                    )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-                },
-                '& + .MuiSwitch-track': {
-                    opacity: 1,
-                    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-                },
-            },
-        },
-        '& .MuiSwitch-thumb': {
-            backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
-            width: 32,
-            height: 32,
-            '&:before': {
-                content: "''",
-                position: 'absolute',
-                width: '100%',
-                height: '100%',
-                left: 0,
-                top: 0,
-                backgroundRepeat: 'no-repeat',
-                backgroundPosition: 'center',
-                backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-                    '#fff',
-                )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-            },
-        },
-        '& .MuiSwitch-track': {
-            opacity: 1,
-            backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
-            borderRadius: 20 / 2,
-        },
-    }));
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 
 const Drawer = dynamic(
@@ -74,12 +25,12 @@ const Drawer = dynamic(
 )
 
 export default function Header({ props }) {
-
     const [propsAvailable, setPropsAvailable] = useState(false);
     const [allWorkshops, setAllWorkshops] = useState([]);
     const [allGuides, setAllGuides] = useState([]);
     const [allInsights, setAllInsights] = useState([]);
     const { theme, setTheme } = useTheme()
+    const [darkMode, setDarkMode] = useState();
 
     useEffect(() => {
         if (props && props.workshops) {
@@ -90,7 +41,14 @@ export default function Header({ props }) {
         }
     }, [props.workshops])
 
-    const drawerWidth = 240;
+    useEffect(() => {
+        if(theme === 'dark') {
+            setDarkMode(true)
+        } else {
+            setDarkMode(false)
+        }
+    }, [theme])
+
 
     const themeToggle = (e) => {
         if (theme === 'light') {
@@ -174,17 +132,11 @@ export default function Header({ props }) {
                             <Link href='/Glossary' passHref>Glossary</Link>
                         </li>
                     </ul>
-{/* 
-                    <MaterialUISwitch
-                        // value could be 'dark' or 'light'
-                        checked={theme == 'dark'}
+                    <IconButton sx={{ ml: 1 }}
                         onClick={(e) => themeToggle(e)}
-                    /> */}
-                    <IconButton sx={{ ml: 1 }} 
-                    onClick={(e) => themeToggle(e)}
-                    color="inherit">
-        {theme === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
+                        color="inherit">
+                        {darkMode === true ? <Brightness7Icon /> : <DarkModeIcon />}
+                    </IconButton>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
